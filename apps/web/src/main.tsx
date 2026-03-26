b0a7691a16ec79ff3b986f1e3d1ffe3d5d2a50cd
Thiago Fernandes <mecpro.app@gmail.com>
2026-03-26 22:29:34 +0100

    perf: implementa lazy loading e cache com React Query no frontend

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,19 +2,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import { NoCursorWrapper } from "./components/NoCursorWrapper"; // 👈 import do wrapper
+import { NoCursorWrapper } from "./components/NoCursorWrapper";
 import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutos
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <NoCursorWrapper> {/* 👈 aplica o no-cursor global */}
-          <App />
-        </NoCursorWrapper>
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <NoCursorWrapper>
+            <App />
+          </NoCursorWrapper>
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
